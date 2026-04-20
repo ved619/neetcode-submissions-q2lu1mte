76c8df40070401f4e1769277fed2e5867378a3b9
ved619 <58264367+ved619@users.noreply.github.com>
2026-04-20 13:55:29 +0530

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+ public:
+  bool isAlienSorted(vector<string>& words, string order) {
+    // step 1: create mapping
+    vector<int> rank(26);
+    for (int i = 0; i < order.size(); ++i) {
+      rank[order[i] - 'a'] = i;
+    }
+    // step 2: compare adjacent words
+    for (int i = 0; i < words.size() - 1; ++i) {
+      string w1 = words[i];
+      string w2 = words[i + 1];
+      int j = 0;
+      while (j < w1.size() and j < w2.size()) {
+        if (w1[j] != w2[j]) {
+          // compare using alien order
+          if (rank[w1[j] - 'a'] > rank[w2[j] - 'a']) {
+            return false;
+          }
+          break;
+        }
+        ++j;
+      }
+      // prefix case
+      if (j == w2.size() and w1.size() > w2.size()) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
