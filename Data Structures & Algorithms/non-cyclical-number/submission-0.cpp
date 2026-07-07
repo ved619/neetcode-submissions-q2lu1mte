5daf0ec9f92166d99c304cd9a5aa2df7b37b3fb9
ved619 <58264367+ved619@users.noreply.github.com>
2026-07-07 20:31:31 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+ public:
+  int nextNumber(int n) {
+    int sum = 0;
+    while (n > 0) {
+      int digit = n % 10;
+      sum += digit * digit;
+      n /= 10;
+    }
+    return sum;
+  }
+  bool isHappy(int n) {
+    unordered_set<int> seen;
+    while (n != 1) {
+      if (seen.count(n)) {
+        return false;
+      }
+      seen.insert(n);
+      n = nextNumber(n);
+    }
+    return true;
+  }
+};
