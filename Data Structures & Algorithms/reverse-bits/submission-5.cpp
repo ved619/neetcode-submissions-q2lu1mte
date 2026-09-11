94f27545b0eb5ed78891dc2768d0087b40ecb2f0
ved619 <58264367+ved619@users.noreply.github.com>
2026-09-11 14:59:11 +0530

    Add: reverse-bits - submission-5

---
+++ Data Structures & Algorithms/reverse-bits/submission-5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+ public:
+  uint32_t reverseBits(uint32_t n) {
+    int result = 0;
+    for (int i = 0; i < 32; ++i) {
+      int lastBit = n & 1;
+      result = result << 1;
+      result = result | lastBit;
+      n = n >> 1;
+    }
+    return result;
+  }
+};
