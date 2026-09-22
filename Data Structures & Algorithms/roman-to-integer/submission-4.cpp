73786b9c18fb3a3d430dbc502c30a858fa3935fd
ved619 <58264367+ved619@users.noreply.github.com>
2026-09-22 20:31:44 +0530

    Add: roman-to-integer - submission-4

---
+++ Data Structures & Algorithms/roman-to-integer/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+ public:
+  int romanToInt(string s) {
+    map<char, int> mp;
+    mp.insert({'I', 1});
+    mp.insert({'V', 5});
+    mp.insert({'X', 10});
+    mp.insert({'L', 50});
+    mp.insert({'C', 100});
+    mp.insert({'D', 500});
+    mp.insert({'M', 1000});
+
+    int result = 0;
+    for (int i = 0; i < s.length(); ++i) {
+      if (i + 1 < s.length() and mp[s[i]] < mp[s[i + 1]]) {
+        result -= mp[s[i]];
+      } else {
+        result += mp[s[i]];
+      }
+    }
+    return result;
+  }
+};
